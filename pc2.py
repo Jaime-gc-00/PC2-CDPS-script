f07ee9800095e390c29b61023e9732b00cf7362b
Jaime-gc-00 <78904735+Jaime-gc-00@users.noreply.github.com>
2022-01-16 18:38:36 +0100

    Update pc2.py

--- pc2.py
+++ pc2.py
@@ -8,28 +8,11 @@ import subprocess
 import json
 import os
 
-cwd = str(os.getcwd())
-puerto_de_arranque = str(2022)
-
-#CREAMOS LA VARIABLE DE ENTORNO
-os.environ['GROUP_NUMBER']="Equipo 40"
 #OBTENEMOS EL VALOR DE LA VARIABLE DE ENTORNO
 numero_de_grupo = str(os.environ.get('GROUP_NUMBER'))
 
 call(['sudo apt-get update'], shell=True)
 call(['sudo apt-get install nano'], shell=True)
-call(['sudo apt-get install pip'], shell=True)
-call(["sudo sudo apt-get install python3-pip"], shell=True)
-
-#Descarga de la aplicacion 
-call(['sudo sudo apt-get install nodejs'], shell=True)
-call(['sudo sudo apt-get install npm'], shell=True)
-call(['sudo git clone https://github.com/CDPS-ETSIT/practica_creativa2.git'], shell=True)
-
-#INSTALACION DE REQUIRMENTS CON PIP
-requirements = open(cwd+"/practica_creativa2/bookinfo/src/productpage/requirements.txt", 'r')
-for line in requirements:
-    call(["pip3 install " + line ], shell=True)
 
 #MODIFICA EL TITULO Y ARRANCA LA APLICACION EN EL PUERTO :9999
 call(['cp '+cwd+'/practica_creativa2/bookinfo/src/productpage/templates/productpage.html '+cwd+'/practica_creativa2/bookinfo/src/productpage/templates/productpage2.html'], shell=True)
@@ -43,4 +26,4 @@ for line in copia:
 f.close()
 copia.close()
 call(['rm '+cwd+'/practica_creativa2/bookinfo/src/productpage/templates/productpage2.html'],shell=True)
-call(['python3 '+cwd+'/practica_creativa2/bookinfo/src/productpage/productpage_monolith.py ' + puerto_de_arranque ], shell=True)
+
